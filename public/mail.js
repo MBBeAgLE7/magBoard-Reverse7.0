9324e26690dc638b72875e0f68caa815ed83d6ad
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2024-02-28 17:19:38 +0530

    commited
    hi

---
+++ public/mail.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader-1");
+    loader.classList.add("loader-1--hidden");
+    loader.addEventListener("transitioned", () =>{
+        document.body.removeChild(loader);
+    })
+})
+  
